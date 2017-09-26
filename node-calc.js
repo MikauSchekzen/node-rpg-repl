8f8d59e92e7703b4f9a986238c3626143f6e757b
Tijmen Bok <mikauschekzen@gmail.com>
2017-09-27 01:16:06 +0200

    initial commit

---
+++ node-calc.js
@@ -0,0 +1,65 @@
+const dr = require("./dice-roller.js");
+var dice = new dr.DiceRoller();
+var lastRoll = null;
+const readline = require("readline");
+
+var outputRaw = true;
+var help = {
+	basic: "Define what you want help with.\n\n" +
+		"'help commands' - List special commands\n" +
+		"\n",
+	commands: "Possible commands (next to default JS and NodeJS ones):\n" +
+		"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n" +
+		"'roll <cmd>' - Replace <cmd> with something like '1d6' or '2d10+15'\n\n"
+};
+
+function showHelp(about) {
+	if(about === "basic") {
+		process.stdout.write(help.basic);
+	}
+	else if(about.match(/(?:COMMANDS?|CMDS?)/i)) {
+		process.stdout.write(help.commands);
+	}
+};
+
+function roll(cmd) {
+  outputRaw = false;
+  lastRoll = dice.roll(cmd);
+  console.log(lastRoll.toString());
+	outputRaw = true;
+};
+
+function defaultCommand(line) {
+	if(outputRaw) console.log(eval(line));
+	else eval(line);
+};
+
+process.stdout.write("NodeJS RPG Dice Roller loaded.\n\n" + help.commands);
+
+var rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+rl.prompt();
+
+rl.on("line", function(line) {
+	if(line.match(/^EXIT$/i)) {
+		rl.close();
+	}
+	else if(line.match(/^HELP$/i)) {
+		showHelp("basic");
+	}
+	else if(line.match(/^HELP[ ]([a-zA-Z0-9]+)$/i)) {
+		showHelp(RegExp.$1);
+	}
+	else if(line.match(/^R(?:OLL)?[ ]([0-9d\+\-\*\/]+)$/i)) {
+		roll(RegExp.$1);
+	}
+	else defaultCommand(line);
+	rl.prompt();
+});
+rl.on("close", function() {
+	process.stdout.write("\n");
+	process.exit(0);
+});
