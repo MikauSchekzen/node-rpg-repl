6f4cf5548b1946fe0e8e5a726cf73368a121b121
Tijmen Bok <mikauschekzen@gmail.com>
2018-12-31 16:50:07 +0100

    add clear command

--- node-calc.js
+++ node-calc.js
@@ -6,103 +6,107 @@ const clipboard = require("clipboardy");
 
 require("./utils");
 
-process.stdout.write("\033c");
+clearTerminal();
 
 var outputRaw = true;
 var lastResult = "";
 var help = {
-    basic: "Define what you want help with.\n\n" +
-    "'help commands' - List special commands\n" +
-    "\n",
-    commands: "Possible commands (next to default JS and NodeJS ones):\n" +
-    getSeparatorLine() + "\n" +
-    "'roll <cmd>' - Replace <cmd> with something like '1d6' or '2d10+15'\n\n"
+  basic: "Define what you want help with.\n\n" +
+  "'help commands' - List special commands\n" +
+  "\n",
+  commands: "Possible commands (next to default JS and NodeJS ones):\n" +
+  getSeparatorLine() + "\n" +
+  "'roll <cmd>' - Replace <cmd> with something like '1d6' or '2d10+15'\n\n"
 };
 
+function clearTerminal() {
+  process.stdout.write("\033c");
+}
+
 function getSeparatorLine() {
-    let a = 0;
-    let cols = process.stdout.columns;
-    let str = "";
-    while(a < cols) {
-        if(a % 2 === 0) str += "=";
-        else str += "-"
-        a++
-    }
-    return str;
+  let a = 0;
+  let cols = process.stdout.columns;
+  let str = "";
+  while(a < cols) {
+    if(a % 2 === 0) str += "=";
+    else str += "-"
+    a++
+  }
+  return str;
 }
 
 function showHelp(about) {
-    if(about === "basic") {
-        process.stdout.write(help.basic);
-    }
-    else if(about.match(/(?:COMMANDS?|CMDS?)/i)) {
-        process.stdout.write(help.commands);
-    }
+  if(about === "basic") {
+    process.stdout.write(help.basic);
+  }
+  else if(about.match(/(?:COMMANDS?|CMDS?)/i)) {
+    process.stdout.write(help.commands);
+  }
 };
 
 function roll(cmd) {
-    // Don't output this raw
-    outputRaw = false;
-    // Roll
-    lastRoll = dice.roll(cmd);
-    // Log result
-    console.log(lastRoll.toString());
-    // Save last result
-    lastResult = lastRoll.getTotal().toString();
-    // Revert to raw outputting
-    outputRaw = true;
+  // Don't output this raw
+  outputRaw = false;
+  // Roll
+  lastRoll = dice.roll(cmd);
+  // Log result
+  console.log(lastRoll.toString());
+  // Save last result
+  lastResult = lastRoll.getTotal().toString();
+  // Revert to raw outputting
+  outputRaw = true;
 };
 
 function defaultCommand(line) {
-    if(outputRaw) {
-        try {
-            lastResult = eval(line);
-            if(typeof lastResult === "number") {
-                lastResult = lastResult.toString();
-            }
-            console.log(lastResult);
-        }
-        catch(err) {
-            console.error(err);
-        }
+  if(outputRaw) {
+    try {
+      lastResult = eval(line);
+      if(typeof lastResult === "number") {
+        lastResult = lastResult.toString();
+      }
+      console.log(lastResult);
     }
-    else {
-        try {
-            eval(line);
-        }
-        catch(err) {
-            console.error(err);
-        }
+    catch(err) {
+      console.error(err);
     }
+  }
+  else {
+    try {
+      eval(line);
+    }
+    catch(err) {
+      console.error(err);
+    }
+  }
 };
 
 function handleKeyPress(key, ev) {
-    if(ev.ctrl) {
-        // Copy
-        if(ev.name === "c") {
-            copyToClipboard();
-        }
-        // Paste
-        else if(ev.name === "v") {
-            pasteFromClipboard();
-        }
+  if(ev.ctrl) {
+    // Copy
+    if(ev.name === "c") {
+      copyToClipboard();
     }
+    // Paste
+    else if(ev.name === "v") {
+      pasteFromClipboard();
+    }
+  }
 };
 
 function copyToClipboard() {
-    clipboard.write(lastResult);
+  clipboard.write(lastResult);
 };
 
 function pasteFromClipboard() {
-    var content = clipboard.readSync();
-    rl.write(content);
+  var content = clipboard.readSync();
+  rl.write(content);
 };
 
 process.stdout.write("NodeJS RPG Dice Roller loaded.\n\n");
 
 let rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 
 rl.input.on("keypress", handleKeyPress);
@@ -113,22 +117,25 @@ rl.on("SIGINT", function() {});
 rl.prompt();
 
 rl.on("line", function(line) {
-    if(line.match(/^EXIT$/i)) {
-        rl.close();
-    }
-    else if(line.match(/^HELP$/i)) {
-        showHelp("basic");
-    }
-    else if(line.match(/^HELP[ ]([a-zA-Z0-9]+)$/i)) {
-        showHelp(RegExp.$1);
-    }
-    else if(line.match(/^R(?:OLL)?[ ](.+)$/i)) {
-        roll(RegExp.$1);
-    }
-    else defaultCommand(line);
-    rl.prompt();
+  if(line.match(/^EXIT$/i)) {
+    rl.close();
+  }
+  else if(line.match(/^CLEAR$/i)) {
+    clearTerminal();
+  }
+  else if(line.match(/^HELP$/i)) {
+    showHelp("basic");
+  }
+  else if(line.match(/^HELP[ ]([a-zA-Z0-9]+)$/i)) {
+    showHelp(RegExp.$1);
+  }
+  else if(line.match(/^R(?:OLL)?\s(.+)$/i)) {
+    roll(RegExp.$1);
+  }
+  else defaultCommand(line);
+  rl.prompt();
 });
 rl.on("close", function() {
-    process.stdout.write("\n");
-    process.exit(0);
+  process.stdout.write("\n");
+  process.exit(0);
 });
